93f5a298d42adf895d501657aa1960a17d1c3215
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-06 17:34:23 +0200

    Creazione in index.html di: - Stampa dati del biglietto Creazione in function.js di: - Output dati del biglietto errati - Output dati del biglietto

--- function.js
+++ function.js
@@ -1,36 +1,40 @@
 //Creazione della funzione per il calcolo del biglietto del treno
 function calcoloBiglietto() {
-//Recupero dati
-const km = parseFloat(document.getElementById("km").value);
-const eta = parseInt(document.getElementById("età").value);
+    //Recupero dati
+    const km = parseFloat(document.getElementById("km").value);
+    const eta = parseInt(document.getElementById("età").value);
 
-//Variabile del prezzo per ogni chilometro
-const prezzoAlKm = 0.21;
+    //Variabile del prezzo per ogni chilometro
+    const prezzoAlKm = 0.21;
 
-//Verifica inserimento dei dati
-if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
-    console.log("Dati non validi");
-} else {
+    //Verifica inserimento dei dati
+    if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
+        //Output dati del biglietto errati
+        document.getElementById("output_km").innerText = "Dati non validi";
+        document.getElementById("output_eta").innerText = "Dati non validi";
+        document.getElementById("output_sconto").innerText = "Dati non validi";
+        document.getElementById("output_prezzo").innerText = "Dati non validi";
+    } else {
 
-    //Calcolo del prezzo del biglietto senza sconti applicati
-    let prezzoBiglietto = km * prezzoAlKm;
+        //Calcolo del prezzo del biglietto senza sconti applicati
+        let prezzoBiglietto = km * prezzoAlKm;
 
-    //Variabile di controllo sconti
-    let tipoSconto = "Nessuno sconto";
+        //Variabile di controllo sconti
+        let tipoSconto = "Nessuno sconto";
 
-    //Controllo dell'età per l'applicazione degli sconti tramite IF
-    if (eta < 18) {
-        prezzoBiglietto = prezzoBiglietto * 0.8;
-        tipoSconto = "Sconto minorenni (20%)";
-    } else if (eta > 65) {
-        prezzoBiglietto = prezzoBiglietto * 0.6;
-        tipoSconto = "Sconto over 65 (40%)";
+        //Controllo dell'età per l'applicazione degli sconti tramite IF
+        if (eta < 18) {
+            prezzoBiglietto = prezzoBiglietto * 0.8;
+            tipoSconto = "Sconto minorenni (20%)";
+        } else if (eta > 65) {
+            prezzoBiglietto = prezzoBiglietto * 0.6;
+            tipoSconto = "Sconto over 65 (40%)";
+        }
+
+        //Output dati del biglietto
+        document.getElementById("output_km").innerText = km;
+        document.getElementById("output_eta").innerText = eta;
+        document.getElementById("output_sconto").innerText = tipoSconto;
+        document.getElementById("output_prezzo").innerText = `€${prezzoBiglietto.toFixed(2)}`;
     }
-    
-    //Consol.log dei dati importanti
-    console.log(`Chilometri: ${km}`);
-    console.log(`Età: ${eta}`);
-    console.log(`Sconto: ${tipoSconto}`);
-    console.log(`Prezzo finale: €${prezzoBiglietto.toFixed(2)}`);
-}
 }
