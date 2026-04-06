225aa6811ef7ac795a48cd21da86c685969fcab7
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-06 16:05:15 +0200

    Creazione in index.js di: - Variabile per il calcolo del prezzo dei biglietti - Variabile di controllo per sconti - Controllo dell'età tramite IF

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ const prezzoAlKm = 0.21;
 //Verifica inserimento dei dati
 if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
     console.log("Dati non validi");
-} 
+} else {
+
+    //Calcolo del prezzo del biglietto senza sconti applicati
+    let prezzoBiglietto = km * prezzoAlKm;
+
+    //Variabile di controllo sconti
+    let tipoSconto = "Nessuno sconto";
+
+    //Controllo dell'età per l'applicazione degli sconti tramite IF
+    if (eta < 18) {
+        prezzoBiglietto = prezzoBiglietto * 0.8;
+        tipoSconto = "Sconto minorenni (20%)";
+    } else if (eta > 65) {
+        prezzoBiglietto = prezzoBiglietto * 0.6;
+        tipoSconto = "Sconto over 65 (40%)";
+    }
+}
