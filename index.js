bbf5eb6eee920617c43e469ae809211e0b451c68
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-06 17:11:52 +0200

    Creazione in index.js di: - Variabile per il recupero del bottone creato in index.html - Evento che permette l'attivazione della funzione calcooloBiglietto tramite click del bottone

---
+++ index.js
@@ -0,0 +1,5 @@
+// Recupero l'elemento HTML bottone tramite id "calcola"
+const bottone = document.getElementById("calcola");
+
+//Attivazione della funzione calcoloBiglietto al click sul bottone calcola
+bottone.addEventListener("click", calcoloBiglietto);
