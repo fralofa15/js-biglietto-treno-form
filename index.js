271a190e076fadb35dbb10f150021cbafe9b4016
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-06 16:02:12 +0200

    Creazione in index.js di: - Controllo inserimento corretto dei dati

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const eta = parseInt(prompt("Inserisci l'età del passeggero"));
 
 //Variabile del prezzo per ogni chilometro
 const prezzoAlKm = 0.21;
+
+//Verifica inserimento dei dati
+if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
+    console.log("Dati non validi");
+} 
