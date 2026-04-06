6a861ccbc2501a030b432aeeae7dd702ab0e7f39
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-06 15:59:34 +0200

    Creazione in index.js di: - Prompt per utente - Variabile prezzo

---
+++ index.js
@@ -0,0 +1,6 @@
+//Creazone prompt per utente
+const km = parseFloat(prompt("Inserisci il numero di chilometri da percorrere"));
+const eta = parseInt(prompt("Inserisci l'età del passeggero"));
+
+//Variabile del prezzo per ogni chilometro
+const prezzoAlKm = 0.21;
