d71fa55be57604e5d87839b9422f7576a0b90aec
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-06 16:43:31 +0200

    Spostamento del codice presente in index.js nel nuovo file function.js Aggiunto collegamento in index.html del file function.js

---
+++ function.js
@@ -0,0 +1,33 @@
+//Creazone prompt per utente
+const km = parseFloat(prompt("Inserisci il numero di chilometri da percorrere"));
+const eta = parseInt(prompt("Inserisci l'età del passeggero"));
+
+//Variabile del prezzo per ogni chilometro
+const prezzoAlKm = 0.21;
+
+//Verifica inserimento dei dati
+if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
+    console.log("Dati non validi");
+} else {
+
+    //Calcolo del prezzo del biglietto senza sconti applicati
+    let prezzoBiglietto = km * prezzoAlKm;
+
+    //Variabile di controllo sconti
+    let tipoSconto = "Nessuno sconto";
+
+    //Controllo dell'età per l'applicazione degli sconti tramite IF
+    if (eta < 18) {
+        prezzoBiglietto = prezzoBiglietto * 0.8;
+        tipoSconto = "Sconto minorenni (20%)";
+    } else if (eta > 65) {
+        prezzoBiglietto = prezzoBiglietto * 0.6;
+        tipoSconto = "Sconto over 65 (40%)";
+    }
+    
+    //Consol.log dei dati importanti
+    console.log(`Chilometri: ${km}`);
+    console.log(`Età: ${eta}`);
+    console.log(`Sconto: ${tipoSconto}`);
+    console.log(`Prezzo finale: €${prezzoBiglietto.toFixed(2)}`);
+}
